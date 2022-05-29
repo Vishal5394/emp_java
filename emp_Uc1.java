24795bbd2499bb280013f611ca9a84561a8a16b2
vishal5394 <vishaljadyal@gmail.com>
2022-05-29 10:46:02 +0530

    Emp_Attendance

---
+++ emp_Uc1.java
@@ -0,0 +1,14 @@
+package employee_wage;
+
+public class emp_Uc1 {
+
+	public static void main(String[] args) {
+		int is_present= 1;
+		double emp_attendance = Math.floor(Math.random ()* 10) % 2;
+		if ( emp_attendance == is_present )
+		System.out.println("Employee is present");
+		else
+			System.out.println("Employee is Absent");
+		
+	}
+}
